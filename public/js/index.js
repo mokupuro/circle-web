8b993f029caf3ea3210b526d02ff83b9d6419105
tamaki <hbk2002320@gmail.com>
2021-03-28 15:15:37 +0900

    ヘッダーのハンバガーメニューのアクションの追加

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+// ハンバガーメニュー
+document.addEventListener('DOMContentLoaded', () => {
+
+  // "navbar-burger" を取得
+  const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  if (navbarBurgers.length > 0) {
+
+    // クリックイベントを追加
+    navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // data-target属性から値を取得
+        const target = el.dataset.target;
+        const navMenu = document.getElementById(target);
+
+        // ""navbar-burger"と"navbar-menu"にis-active" を追加
+        el.classList.toggle('is-active');
+        navMenu.classList.toggle('is-active');
+      });
+    });
+  }
+
+});
